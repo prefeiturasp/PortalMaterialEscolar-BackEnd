ee510a8d94bd99eba32f1ab02c543f660623d891
Kelwy <kelwyduarte@gmail.com>
2020-07-06 17:39:42 -0300

    Adiciona modelo de anexos

---
+++ sme_material_apps/proponentes/models/anexo.py
@@ -0,0 +1,37 @@
+from auditlog.models import AuditlogHistoryField
+from auditlog.registry import auditlog
+from django.db import models
+
+from sme_material_apps.core.models_abstracts import ModeloBase
+from .proponente import Proponente
+from .tipo_documento import TipoDocumento
+
+
+class Anexo(ModeloBase):
+    historico = AuditlogHistoryField()
+
+    proponente = models.ForeignKey(Proponente, on_delete=models.CASCADE, blank=True, null=True, related_name='anexos')
+
+    arquivo = models.FileField()
+
+    tipo_documento = models.ForeignKey(
+        TipoDocumento,
+        on_delete=models.PROTECT,
+        blank=True, null=True,
+        related_name='anexos'
+    )
+
+    def as_dict(self):
+        return {
+            "proponente": self.proponente.uuid,
+            "arquivo": self.arquivo,
+            "tipo_documento": self.tipo_documento,
+            "uuid": self.uuid
+        }
+
+    class Meta:
+        verbose_name = "Anexo"
+        verbose_name_plural = "Anexos"
+
+
+auditlog.register(Anexo)
