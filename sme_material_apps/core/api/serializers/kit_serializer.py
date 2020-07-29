86a59cfb36e56ef4e29ee3c4ed84b30639ebc48b
Kelwy <kelwyduarte@gmail.com>
2020-07-29 11:38:35 -0300

    Altera admin de kit e itens de material
    Adiciona preço maximo no kit e oculta preço maximo do item de material
    no admin.

--- sme_material_apps/core/api/serializers/kit_serializer.py
+++ sme_material_apps/core/api/serializers/kit_serializer.py
@@ -9,4 +9,4 @@ class KitSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Kit
-        fields = ('uuid', 'nome', 'materiais_do_kit', 'ativo')
+        fields = ('uuid', 'nome', 'materiais_do_kit', 'preco_maximo', 'ativo')
