a9c24f11ff6dd651774080a75b6880bf76a0161e
Calvin Rossinhole <calvin.masters@gmail.com>
2020-08-10 11:20:49 -0300

    endpoint reset senha

--- sme_material_apps/custom_user/api/serializers.py
+++ sme_material_apps/custom_user/api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from rest_framework import serializers
 
 from sme_material_apps.custom_user.models import User
@@ -6,7 +7,15 @@ from sme_material_apps.proponentes.api.serializers.proponente_serializer import 
 
 class UserSerializer(serializers.ModelSerializer):
     proponente = ProponenteSerializer()
+    token = serializers.SerializerMethodField()
+
+    def get_token(self, obj):
+        if not obj.last_login:
+            token_generator = PasswordResetTokenGenerator()
+            novo_token = token_generator.make_token(obj)
+            return novo_token
+        return None
 
     class Meta:
         model = User
-        fields = ("email", "first_name", "proponente")
+        fields = ("id", "email", "first_name", "proponente", "last_login", "token")
