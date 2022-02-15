37075198fd652160583f1f39bfa6c33912a3ffe7
jvictor-am <joaovictorengp@gmail.com>
2022-02-15 18:50:52 -0300

    Feature: ajustar path do arquivo no campo de comprovante_end em LojaSerializer

--- sme_material_apps/proponentes/api/serializers/loja_serializer.py
+++ sme_material_apps/proponentes/api/serializers/loja_serializer.py
@@ -1,13 +1,21 @@
+import environ
 from rest_framework import serializers
 
 from ...models import Loja
 
+env = environ.Env()
+SERVER_NAME = f'{env("SERVER_NAME")}'
+
 
 class LojaSerializer(serializers.ModelSerializer):
     email = serializers.SerializerMethodField()
+    comprovante_end = serializers.SerializerMethodField('get_comprovante_end')
 
     def get_email(self, obj):
         return obj.proponente.email
+
+    def get_comprovante_end(self, obj):
+        return '%s%s' % (SERVER_NAME, obj.comprovante_end.url)
 
     class Meta:
         model = Loja
